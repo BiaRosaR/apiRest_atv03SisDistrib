e2399699372e61e81fcdbb99cd78844a887c8542
BiaRosaR <beatriz.rocha@sptech.school>
2025-05-26 19:37:29 -0300

    Primeiro commit da API Fórmula 1

---
+++ formula1/src/main/java/sptech/school/formula1/PilotoController.java
@@ -0,0 +1,34 @@
+package sptech.school.formula1;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pilotos")
+public class PilotoController {
+    private final PilotoRepository repository;
+
+    public PilotoController(PilotoRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Piloto> listar(){
+        return  repository.findAll();
+    }
+
+    @PostMapping
+    public Piloto adicionar(@RequestBody Piloto piloto) {
+        return repository.save(piloto);
+    }
+
+    @GetMapping("/{nome}")
+    public Piloto buscarPorNome(@PathVariable String nome){
+        Piloto piloto = repository.findByNome(nome).orElse(null);
+        if(piloto == null ){
+            throw new RuntimeException("Piloto não encontrado");
+        }
+        return piloto;
+    }
+}
