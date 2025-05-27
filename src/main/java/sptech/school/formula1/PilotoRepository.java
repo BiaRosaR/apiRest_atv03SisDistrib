661d10ba82b91f21ade0d3b9ad4b57856650d9c1
BiaRosaR <beatriz.rocha@sptech.school>
2025-05-26 23:56:21 -0300

    arquivos da api formula1

---
+++ src/main/java/sptech/school/formula1/PilotoRepository.java
@@ -0,0 +1,10 @@
+package sptech.school.formula1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface PilotoRepository extends JpaRepository<Piloto, Long> {
+    Optional<Piloto> findByNome(String nome);
+}
