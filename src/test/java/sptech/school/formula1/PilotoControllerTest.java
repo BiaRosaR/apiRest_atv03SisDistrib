1ff51a497617f94dac9129f767868af393a0eefd
BiaRosaR <beatriz.rocha@sptech.school>
2025-05-27 21:15:25 -0300

    incluindo PilotoControllerTest.java definitivo

--- src/test/java/sptech/school/formula1/PilotoControllerTest.java
+++ src/test/java/sptech/school/formula1/PilotoControllerTest.java
@@ -1,4 +1,20 @@
+package sptech.school.formula1;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PilotoControllerTest {
-  
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void adicionar() {
+    }
+
+    @Test
+    void buscarPorNome() {
+    }
 }
